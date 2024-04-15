3fff831030d8c6392cd0fab0f8d1732cebde7fb8
Nyiranez <nyiranezalouise93@gmail.com>
2024-04-15 12:45:21 +0200

    filter by include added

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -38,12 +38,18 @@ export const Home = () => {
   const [filteredperson, setFilteredPerson] = useState(Persons);
   const handleFilter = (event) => {
     const value = event.target.value;
-    const filtered = filteredperson.filter(user => user.name.includes(value));
-    setFilteredPerson(filtered);
+    if (value === " " || value === undefined) {
+      setFilteredPerson(Persons);
+    }
+    else {
+      const filtered = filteredperson.filter(user => user.name.includes(value));
+      setFilteredPerson(filtered);
+    }
+
   };
 
   const slic = Persons.slice(0, 3)
-  
+
   return (
     <div className="">
       <h1 className='text-4xl underline '>Slice</h1>
@@ -59,15 +65,15 @@ export const Home = () => {
 
       <h1 className='text-4xl underline '>Filter</h1>
 
-    
-      <input type="text" onChange={handleFilter} className='border-4 ' placeholder='Enter Name'/>
-    {filteredperson.map(user => (
-      <div key={user.name}>
-        <p>Name: {user.name}</p>
-        <p>Age: {user.age}</p>
-        <p>Address: {user.address}</p>
-      </div>
-    ))}
+
+      <input type="text" onChange={handleFilter} className='border-4 ' placeholder='Enter Name' />
+      {filteredperson.map(user => (
+        <div key={user.name}>
+          <p>Name: {user.name}</p>
+          <p>Age: {user.age}</p>
+          <p>Address: {user.address}</p>
+        </div>
+      ))}
 
     </div>
   )
